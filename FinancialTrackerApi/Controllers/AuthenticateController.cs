ca4fa54b5609168a4335777a1e854314a5a8cbb7
scott-woods <scott_woods44@yahoo.com>
2023-07-22 21:02:18 -0400

    .

--- FinancialTrackerApi/Controllers/AuthenticateController.cs
+++ FinancialTrackerApi/Controllers/AuthenticateController.cs
@@ -27,12 +27,6 @@ namespace FinancialTrackerApi.Controllers
             var domain = _config.GetValue<string>("Auth0:Domain");
             var audience = _config.GetValue<string>("Auth0:Audience");
 
-            //var client = new RestClient("https://dev-m14zuewx4kqgiljh.us.auth0.com/oauth/token");
-            //var request = new RestRequest("", Method.Post);
-            //request.AddHeader("content-type", "application/json");
-            //request.AddParameter("application/json", "{\"client_id\":\"fAeiWLBnsayueWALLF8i3ANqsYrLVd1R\",\"client_secret\":\"08wDYhdvM2nV8N9VWkIBX_LJPGspgaGwaaIug3kHaX1jQA7kayvyLTSYumnw2Ucw\",\"audience\":\"https://localhost:7190/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
-            //RestResponse response = client.Execute(request);
-
             var client = new RestClient($"https://{domain}/oauth/token");
             var request = new RestRequest("", Method.Post);
             request.AddHeader("content-type", "application/x-www-form-urlencoded");
